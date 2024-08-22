c148c97065f5a88805840a8ced713742423c4f9a
Nayara Piauilino <naayaraholanda@gmail.com>
2024-08-22 19:06:52 -0300

    Add end-to-end tests

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -12,7 +12,7 @@ test('sign up successfully', async ({ page }) => {
 
   const toast = page.getByText('Restaurante cadastrado com sucesso!')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('sign up with error', async ({ page }) => {
